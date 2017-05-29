f6f04c88a3b766502a03ae05c49ad927702b39c2
Matthew Skolaut <tech2077@gmail.com>
2017-05-29 16:52:53 -0500

    Add basic web server through Iron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!!");
+    println!("Server Starting!");
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello, World!")))
+    }).http("localhost:8000").unwrap();
 }
